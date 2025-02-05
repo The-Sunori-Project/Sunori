ee9a1386a2c12777dc31e5f5c8801b9362fe2d95
Aric S. Jones <aric.jones@student.msdmartinsville.org>
2025-02-05 13:42:29 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  // Before commit add " basepath: "nextjs.sunori.org/"
+  // Before commit add " basepath: "sunori.org/"
   // If developing remove basepath
+  basepath: "sunori.org/"
   images: {
     unoptimized: true,
   },
