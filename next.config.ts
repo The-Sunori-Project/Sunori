69947dfa9151c226b7aa17829c41b0058fb24bf4
Aric S. Jones <aric.jones@student.msdmartinsville.org>
2025-02-07 10:31:35 -0500

    Remove unnecessary basepath

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  // Before commit add " basepath: "sunori.org/"
-  // If developing remove basepath
   images: {
     unoptimized: true,
   },
