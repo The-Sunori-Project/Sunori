f66b40e538fc8aecc6f6009af134a4a96a9f088c
Aric S. Jones <aric.jones@student.msdmartinsville.org>
2025-02-11 14:29:40 -0500

    Update Card.tsx

--- app/modules/Card/Card.tsx
+++ app/modules/Card/Card.tsx
@@ -1,6 +1,15 @@
 import Link from 'next/link'
 import Image from 'next/image'
-export default function Card: React.FC<MyComponentProps> = ({ Title, Img, Target, Url, Owner, Diamond, Gold }) => {
+interface CardProps {
+    Title?: string;
+    Img?: string;
+    Target?: string;
+    Url?: string;
+    Owner?: boolean;
+    Diamond?: boolean;
+    Gold?: boolean;
+}
+export default const Card: React.FC<CardProps> = ({ Title, Img, Target, Url, Owner, Diamond, Gold }) => {
     return(
         <Link target={props.Target} className={props.Owner ? 'owner' : props.Diamond ? 'diamond' : props.Gold ? 'gold' : 'card'} href={props.Url}>
             <Image className={props.Owner ? 'ownerImg' : props.Diamond ? 'diamondImg' : props.Gold ? 'goldImg' : 'cardImg'} src={"/img/" + props.Img} />
