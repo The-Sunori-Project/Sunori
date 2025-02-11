ba170be14d84fd23b1401bf87e2debc45987328a
Aric S. Jones <aric.jones@student.msdmartinsville.org>
2025-02-11 18:59:12 -0500

    Update Card.tsx

--- app/modules/Card/Card.tsx
+++ app/modules/Card/Card.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import React, { FC } from 'react'
 interface CardProps {
     Title?: string;
     Img?: string;
@@ -9,10 +10,10 @@ interface CardProps {
     Diamond?: boolean;
     Gold?: boolean;
 }
-const Card: React.FC<CardProps> = ({ Title, Img, Target, Url, Owner, Diamond, Gold }) => {
+const Card: FC<CardProps> = ({ Img, Title, Url, Target, Owner, Diamond, Gold }) => {
     return(
         <Link target={props.Target} className={props.Owner ? 'owner' : props.Diamond ? 'diamond' : props.Gold ? 'gold' : 'card'} href={props.Url}>
-            <Image className={props.Owner ? 'ownerImg' : props.Diamond ? 'diamondImg' : props.Gold ? 'goldImg' : 'cardImg'} src={"/img/" + props.Img} />
+            <Image alt={props.title + " Image"} className={props.Owner ? 'ownerImg' : props.Diamond ? 'diamondImg' : props.Gold ? 'goldImg' : 'cardImg'} src={"/img/" + props.Img} />
             <p className={props.Owner ? 'ownerTitle' : props.Diamond ? 'diamondTitle' : props.Gold ? 'goldTitle' : 'cardTitle'}>{props.Title}</p>
         </Link>
     );
