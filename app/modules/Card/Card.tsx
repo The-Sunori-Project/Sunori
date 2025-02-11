e74143de89d22f57456fa9acb14dfb5fc3801ace
Aric S. Jones <aric.jones@student.msdmartinsville.org>
2025-02-11 14:20:05 -0500

    Update Card.tsx

--- app/modules/Card/Card.tsx
+++ app/modules/Card/Card.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
+import Image from 'next/image'
 export default function Card(props){
     return(
         <Link target={props.Target} className={props.Owner ? 'owner' : props.Diamond ? 'diamond' : props.Gold ? 'gold' : 'card'} href={props.Url}>
-            <img className={props.Owner ? 'ownerImg' : props.Diamond ? 'diamondImg' : props.Gold ? 'goldImg' : 'cardImg'} src={"/img/" + props.Img} />
+            <Image className={props.Owner ? 'ownerImg' : props.Diamond ? 'diamondImg' : props.Gold ? 'goldImg' : 'cardImg'} src={"/img/" + props.Img} />
             <p className={props.Owner ? 'ownerTitle' : props.Diamond ? 'diamondTitle' : props.Gold ? 'goldTitle' : 'cardTitle'}>{props.Title}</p>
         </Link>
     );
