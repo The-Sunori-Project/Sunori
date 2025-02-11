fc52bb662752646b7e1d5914bdf6e90918ec653c
Aric S. Jones <aric.jones@student.msdmartinsville.org>
2025-02-11 14:19:25 -0500

    Update Header.tsx

--- app/modules/Header/Header.tsx
+++ app/modules/Header/Header.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
+import Image from 'next/image'
 export default function Header(){
     return(
         <header>
-            <a href="/">
-            <img id="favicon" src="./favicon.ico" width="48px"/>
+            <Link href="/">
+            <Image id="favicon" src="./favicon.ico" width="48px"/>
             <h1 id="title">Sunori</h1>
-            </a>
+            </Link>
             <nav>
                 <ul>
                     <li><Link href="/games">Games</Link></li>
